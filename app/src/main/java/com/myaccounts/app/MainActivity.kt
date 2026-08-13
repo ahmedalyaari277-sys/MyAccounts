63635b055bd87d63154181e8d83c4b2023d77ce7
ahmedalyaari277-sys <ahmedalyaari277@gmail.com>
2026-08-14 00:38:39 +0300

    Update MainActivity.kt

--- app/src/main/java/com/myaccounts/app/MainActivity.kt
+++ app/src/main/java/com/myaccounts/app/MainActivity.kt
@@ -4,76 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import com.myaccounts.app.ui.screens.HomeScreen
-import com.myaccounts.app.ui.screens.PersonAccountScreen
+import androidx.navigation.compose.rememberNavController
+import com.myaccounts.app.ui.navigation.AppNavHost
+import com.myaccounts.app.ui.theme.MyAccountsTheme
 import com.myaccounts.app.ui.viewmodel.LedgerViewModel
+import com.myaccounts.app.ui.viewmodel.LedgerViewModelFactory
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: LedgerViewModel by viewModels()
+    private val viewModel: LedgerViewModel by viewModels {
+        LedgerViewModelFactory(application)
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
         super.onCreate(savedInstanceState)
 
         setContent {
 
-            val personsList by
-                viewModel.personsWithAccounts.collectAsState()
+            MyAccountsTheme {
 
-            var selectedPersonId by remember {
-                mutableStateOf<Long?>(null)
-            }
+                val navController =
+                    rememberNavController()
 
-            if (selectedPersonId == null) {
-
-                HomeScreen(
-
-                    personsList = personsList,
-
-                    onAddPerson = { name, phone, address ->
-
-                        viewModel.addPerson(
-                            name = name,
-                            phone = phone,
-                            address = address
-                        )
-                    },
-
-                    onPersonClick = { personId ->
-
-                        selectedPersonId = personId
-                    }
+                AppNavHost(
+                    navController = navController,
+                    viewModel = viewModel
                 )
-
-            } else {
-
-                val selectedPerson =
-                    personsList.firstOrNull {
-                        it.person.id == selectedPersonId
-                    }
-
-                if (selectedPerson != null) {
-
-                    PersonAccountScreen(
-
-                        personWithAccounts =
-                            selectedPerson,
-
-                        onBack = {
-
-                            selectedPersonId = null
-                        }
-                    )
-
-                } else {
-
-                    selectedPersonId = null
-                }
             }
         }
     }
