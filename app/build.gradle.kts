04b4e9e935e56d324b74764e841026b2a4bf1a95
ahmedalyaari277-sys <ahmedalyaari277@gmail.com>
2026-08-14 16:15:14 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,12 @@ android {
         versionCode = 1
         versionName = "1.0.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables { useSupportLibrary = true }
+        testInstrumentationRunner =
+            "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     compileOptions {
@@ -39,20 +43,58 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
 
-    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    implementation(
+        "androidx.lifecycle:lifecycle-runtime-ktx:2.7.0"
+    )
+
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0"
+    )
+
+    implementation(
+        "androidx.activity:activity-compose:1.8.2"
+    )
+
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2024.02.00"
+        )
+    )
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview"
+    )
+
+    implementation(
+        "androidx.compose.material3:material3"
+    )
+
+    implementation(
+        "androidx.compose.material:material-icons-extended"
+    )
+
+    implementation(
+        "androidx.navigation:navigation-compose:2.7.7"
+    )
 
     val roomVersion = "2.6.1"
-    implementation("androidx.room:room-runtime:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
+
+    implementation(
+        "androidx.room:room-runtime:$roomVersion"
+    )
+
+    implementation(
+        "androidx.room:room-ktx:$roomVersion"
+    )
+
+    ksp(
+        "androidx.room:room-compiler:$roomVersion"
+    )
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
 }
