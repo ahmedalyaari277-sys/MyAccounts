b9353041dfa0b74d318baa5b76ae06ce07dd14d7
ahmedalyaari277-sys <ahmedalyaari277@gmail.com>
2026-08-14 20:13:58 +0300

    Update TransactionRepository.kt

--- app/src/main/java/com/myaccounts/app/data/repository/TransactionRepository.kt
+++ app/src/main/java/com/myaccounts/app/data/repository/TransactionRepository.kt
@@ -1,7 +1,6 @@
 package com.myaccounts.app.data.repository
 
 import com.myaccounts.app.data.local.TransactionEntity
-import com.myaccounts.app.data.local.TransactionType
 import com.myaccounts.app.data.local.dao.TransactionDao
 import kotlinx.coroutines.flow.Flow
 
@@ -12,11 +11,16 @@ class TransactionRepository(
     override suspend fun addTransaction(
         transaction: TransactionEntity
     ): Long {
+
         val transactionId =
-            transactionDao.insertTransaction(transaction)
+            transactionDao.insertTransaction(
+                transaction
+            )
 
         val newBalance =
-            transactionDao.getBalance(transaction.accountId)
+            transactionDao.getBalance(
+                transaction.accountId
+            )
 
         transactionDao.updateCurrencyBalance(
             accountId = transaction.accountId,
@@ -26,43 +30,82 @@ class TransactionRepository(
         return transactionId
     }
 
+    override suspend fun updateTransaction(
+        transaction: TransactionEntity
+    ) {
+
+        transactionDao.updateTransaction(
+            transaction
+        )
+
+        val newBalance =
+            transactionDao.getBalance(
+                transaction.accountId
+            )
+
+        transactionDao.updateCurrencyBalance(
+            accountId = transaction.accountId,
+            balanceMinor = newBalance
+        )
+    }
+
     override fun observeTransactions(
         accountId: Long
     ): Flow<List<TransactionEntity>> {
-        return transactionDao.observeTransactions(accountId)
+
+        return transactionDao.observeTransactions(
+            accountId
+        )
     }
 
     override suspend fun getTransactions(
         accountId: Long
     ): List<TransactionEntity> {
-        return transactionDao.getTransactions(accountId)
+
+        return transactionDao.getTransactions(
+            accountId
+        )
     }
 
     override suspend fun getTransaction(
         transactionId: Long
     ): TransactionEntity? {
-        return transactionDao.getTransaction(transactionId)
+
+        return transactionDao.getTransaction(
+            transactionId
+        )
     }
 
     override fun observeBalance(
         accountId: Long
     ): Flow<Long> {
-        return transactionDao.observeBalance(accountId)
+
+        return transactionDao.observeBalance(
+            accountId
+        )
     }
 
     override suspend fun getBalance(
         accountId: Long
     ): Long {
-        return transactionDao.getBalance(accountId)
+
+        return transactionDao.getBalance(
+            accountId
+        )
     }
 
     override suspend fun deleteTransaction(
         transaction: TransactionEntity
     ) {
-        transactionDao.deleteTransaction(transaction)
+
+        transactionDao.deleteTransaction(
+            transaction
+        )
 
         val newBalance =
-            transactionDao.getBalance(transaction.accountId)
+            transactionDao.getBalance(
+                transaction.accountId
+            )
 
         transactionDao.updateCurrencyBalance(
             accountId = transaction.accountId,
@@ -73,14 +116,22 @@ class TransactionRepository(
     override suspend fun deleteTransactionById(
         transactionId: Long
     ) {
-        val transaction =
-            transactionDao.getTransaction(transactionId)
 
-        transactionDao.deleteTransactionById(transactionId)
+        val transaction =
+            transactionDao.getTransaction(
+                transactionId
+            )
+
+        transactionDao.deleteTransactionById(
+            transactionId
+        )
 
         if (transaction != null) {
+
             val newBalance =
-                transactionDao.getBalance(transaction.accountId)
+                transactionDao.getBalance(
+                    transaction.accountId
+                )
 
             transactionDao.updateCurrencyBalance(
                 accountId = transaction.accountId,
