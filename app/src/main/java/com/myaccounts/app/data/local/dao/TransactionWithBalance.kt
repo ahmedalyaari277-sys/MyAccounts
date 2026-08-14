bf7d7def891f395eb8eee982d9b725a63ef15440
ahmedalyaari277-sys <ahmedalyaari277@gmail.com>
2026-08-14 15:56:56 +0300

    Delete app/src/main/java/com/myaccounts/app/data/local/dao/TransactionWithBalance.kt

--- app/src/main/java/com/myaccounts/app/data/local/dao/TransactionWithBalance.kt
+++
@@ -1,8 +0,0 @@
-package com.myaccounts.app.data.local.dao
-
-import com.myaccounts.app.data.local.entity.TransactionEntity
-
-data class TransactionWithBalance(
-    val transaction: TransactionEntity,
-    val runningBalance: Long
-)
