6b3692ed2d340b4f827f1acf26cd86f7b4da87e8
ahmedalyaari277-sys <ahmedalyaari277@gmail.com>
2026-08-16 01:08:56 +0300

    Remove unused ReportsViewModel from MainActivity

--- app/src/main/java/com/myaccounts/app/MainActivity.kt
+++ app/src/main/java/com/myaccounts/app/MainActivity.kt
@@ -9,17 +9,11 @@ import com.myaccounts.app.ui.navigation.AppNavHost
 import com.myaccounts.app.ui.theme.MyAccountsTheme
 import com.myaccounts.app.ui.viewmodel.LedgerViewModel
 import com.myaccounts.app.ui.viewmodel.LedgerViewModelFactory
-import com.myaccounts.app.ui.viewmodel.ReportsViewModel
-import com.myaccounts.app.ui.viewmodel.ReportsViewModelFactory
 
 class MainActivity : ComponentActivity() {
 
     private val viewModel: LedgerViewModel by viewModels {
         LedgerViewModelFactory(application)
-    }
-
-    private val reportsViewModel: ReportsViewModel by viewModels {
-        ReportsViewModelFactory(application)
     }
 
     override fun onCreate(
