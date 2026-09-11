9d1341f74fb594f322a06ba6899349ee50aeda60
ahmedalyaari277-sys <ahmedalyaari277@gmail.com>
2026-09-11 22:18:00 +0300

    fix(backup): route global and custody backups to separate scopes

--- app/src/main/java/com/myaccounts/app/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/myaccounts/app/ui/navigation/AppNavHost.kt
@@ -16,6 +16,7 @@ import com.myaccounts.app.ui.screens.*
 import com.myaccounts.app.ui.screens.reports.*
 import com.myaccounts.app.ui.theme.AppearanceMode
 import com.myaccounts.app.ui.viewmodel.*
+import com.myaccounts.app.util.BackupScope
 
 @Composable
 fun AppNavHost(navController: NavHostController, viewModel: LedgerViewModel, appearanceMode: AppearanceMode, onAppearanceModeChange: (AppearanceMode) -> Unit) {
@@ -30,17 +31,20 @@ fun AppNavHost(navController: NavHostController, viewModel: LedgerViewModel, app
     val custody: CustodyViewModel = viewModel(factory = CustodyViewModelFactory(app))
     NavHost(navController = navController, startDestination = Routes.GATEWAY) {
         composable(Routes.GATEWAY) { AppGatewayScreen(onAccounts = { navController.navigate(Routes.HOME) }, onCustodies = { navController.navigate(Routes.CUSTODIES) }, onSettings = { navController.navigate(Routes.SETTINGS) }) }
-        composable(Routes.HOME) { HomeScreen(personsList = persons, onAddPerson = { n, p, a, no -> viewModel.addPerson(n, p, a, no) }, onPersonClick = { navController.navigate(Routes.personAccount(it)) }, onQuickTransactionSave = { t, a -> transactions.addTransaction(t, a) }, onReportsClick = { navController.navigate(Routes.REPORTS) }, onArchiveClick = { navController.navigate(Routes.ARCHIVE) }, onBackupRestoreClick = { navController.navigate(Routes.BACKUP_RESTORE) }) }
+        composable(Routes.HOME) { HomeScreen(personsList = persons, onAddPerson = { n, p, a, no -> viewModel.addPerson(n, p, a, no) }, onPersonClick = { navController.navigate(Routes.personAccount(it)) }, onQuickTransactionSave = { t, a -> transactions.addTransaction(t, a) }, onReportsClick = { navController.navigate(Routes.REPORTS) }, onArchiveClick = { navController.navigate(Routes.ARCHIVE) }, onBackupRestoreClick = { navController.navigate(Routes.backupRestore("all")) }) }
         composable(Routes.PERSON_ACCOUNT, arguments = listOf(navArgument("personId") { type = NavType.LongType })) { e -> val id = e.arguments?.getLong("personId"); persons.firstOrNull { it.person.id == id }?.let { p -> PersonAccountScreen(p, { navController.popBackStack() }, { n, ph, a, no -> viewModel.updatePerson(id!!, n, ph, a, no) }, { viewModel.deletePerson(id!!); navController.popBackStack() }, { cur -> navController.navigate(Routes.personReport(id!!, cur)) }, transactions) } }
         composable(Routes.TRANSACTIONS, arguments = listOf(navArgument("accountId") { type = NavType.LongType }, navArgument("currencyCode") { type = NavType.StringType })) { e -> val id = e.arguments?.getLong("accountId"); val c = e.arguments?.getString("currencyCode"); if (id != null && c != null) TransactionScreen(id, c, { navController.popBackStack() }, transactions) }
         composable(Routes.REPORTS) { ReportsScreen(reports, { navController.popBackStack() }, { id -> navController.navigate(Routes.personReport(id, "ALL")) }) }
         composable(Routes.PERSON_REPORT, arguments = listOf(navArgument("personId") { type = NavType.LongType }, navArgument("currencyCode") { type = NavType.StringType })) { e -> val id = e.arguments?.getLong("personId"); if (id != null) PersonReportScreen(id, e.arguments?.getString("currencyCode") ?: "ALL", reports, { navController.popBackStack() }) }
         composable(Routes.ARCHIVE) { ArchiveScreen(archived, { navController.popBackStack() }, { navController.navigate(Routes.archivedPerson(it)) }, { viewModel.restorePerson(it) }, { viewModel.permanentlyDeletePerson(it) }, { viewModel.clearArchive() }, restore, { viewModel.clearRestorePersonResult() }) }
         composable(Routes.ARCHIVED_PERSON, arguments = listOf(navArgument("personId") { type = NavType.LongType })) { e -> val id = e.arguments?.getLong("personId"); archived.firstOrNull { it.person.id == id }?.let { p -> ArchivedPersonDetailScreen(p, { navController.popBackStack() }, { viewModel.restorePerson(id!!); navController.popBackStack(Routes.ARCHIVE, false) }, { viewModel.permanentlyDeletePerson(id!!); navController.popBackStack(Routes.ARCHIVE, false) }) } }
-        composable(Routes.BACKUP_RESTORE) { BackupRestoreScreen { navController.popBackStack() } }
-        composable(Routes.SETTINGS) { SettingsScreen(security, appearanceMode, onAppearanceModeChange, { navController.popBackStack() }, { navController.navigate(Routes.DETAILS) }, { navController.navigate(Routes.BACKUP_RESTORE) }) }
+        composable(Routes.BACKUP_RESTORE, arguments = listOf(navArgument("scope") { type = NavType.StringType; defaultValue = BackupScope.ALL.key })) { e ->
+            val scope = when (e.arguments?.getString("scope")) { BackupScope.CUSTODY.key -> BackupScope.CUSTODY; BackupScope.ACCOUNTS.key -> BackupScope.ACCOUNTS; else -> BackupScope.ALL }
+            BackupRestoreScreen(onBack = { navController.popBackStack() }, scope = scope)
+        }
+        composable(Routes.SETTINGS) { SettingsScreen(security, appearanceMode, onAppearanceModeChange, { navController.popBackStack() }, { navController.navigate(Routes.DETAILS) }, { navController.navigate(Routes.backupRestore("all")) }) }
         composable(Routes.DETAILS) { DetailsScreen { navController.popBackStack() } }
-        composable(Routes.CUSTODIES) { CustodyHomeWithArchiveScreen(custody, { navController.popBackStack() }, { navController.navigate(Routes.custody(it)) }, { navController.navigate(Routes.CUSTODY_ARCHIVE) }, { navController.navigate(Routes.CUSTODY_REPORTS) }, { navController.navigate(Routes.BACKUP_RESTORE) }, { navController.navigate(Routes.CUSTODY_TRANSFER) }) }
+        composable(Routes.CUSTODIES) { CustodyHomeWithArchiveScreen(custody, { navController.popBackStack() }, { navController.navigate(Routes.custody(it)) }, { navController.navigate(Routes.CUSTODY_ARCHIVE) }, { navController.navigate(Routes.CUSTODY_REPORTS) }, { navController.navigate(Routes.backupRestore("custody")) }, { navController.navigate(Routes.CUSTODY_TRANSFER) }) }
         composable(Routes.CUSTODY_ARCHIVE) { CustodyArchiveScreen(custody) { navController.popBackStack() } }
         composable(Routes.CUSTODY_REPORTS) { CustodyReportsScreen(custody) { navController.popBackStack() } }
         composable(Routes.CUSTODY_TRANSFER) { CustodyTransferScreen(custody) { navController.popBackStack() } }
