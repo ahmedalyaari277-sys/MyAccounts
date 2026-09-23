756800ba8033a926ea81279cf2e2e5012107bdc1
ahmedalyaari277-sys <ahmed.mohammed2772010@gmail.com>
2026-09-23 23:56:32 +0300

    feat: expose searchable custody list

--- app/src/main/java/com/myaccounts/app/data/custody/CustodyRepository.kt
+++ app/src/main/java/com/myaccounts/app/data/custody/CustodyRepository.kt
@@ -60,7 +60,7 @@ class CustodyRepository(private val db: com.myaccounts.app.data.local.AppDatabas
         CustodyTransactionType.ORG_LOAN_FROM_OWNER, CustodyTransactionType.ORG_LOAN_REPAYMENT,
         CustodyTransactionType.PERSON_LOAN_TO_OWNER, CustodyTransactionType.OWNER_REPAY_PERSON_LOAN
     )
-    fun observeCustodies(): Flow<List<CustodyEntity>> = dao.observeCustodies()
+    fun observeCustodies(query: String = ""): Flow<List<CustodyEntity>> = dao.observeCustodies(query)
     fun observeCustody(id: Long): Flow<CustodyEntity?> = dao.observeCustody(id)
     fun observePersons(id: Long): Flow<List<CustodyPersonEntity>> = dao.observePersons(id)
     fun observeAccounts(id: Long): Flow<List<CustodyAccountEntity>> = dao.observeAccounts(id)
