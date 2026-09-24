dfe183d0b3d0871e3a1f5659e73d5ef56d568ad7
ahmedalyaari277-sys <ahmed.mohammed2772010@gmail.com>
2026-09-24 03:00:43 +0300

    fix: import search stateIn operator

--- app/src/main/java/com/myaccounts/app/ui/viewmodel/LedgerViewModel.kt
+++ app/src/main/java/com/myaccounts/app/ui/viewmodel/LedgerViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 
